83036299936963a83da0ba60f208aef44b1e0e55
diegooppitz <diegooppitz@gmail.com>
2023-12-28 09:09:05 -0300

    fix router error

--- itau_clone/banking-ui/src/App.js
+++ itau_clone/banking-ui/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 
 function App() {
   return (
     <Router>
       <div>
-        <Route exact path="/" component={Home} />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
       </div>
     </Router>
   );
